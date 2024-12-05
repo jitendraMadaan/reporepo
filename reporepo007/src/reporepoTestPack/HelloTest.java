09ed72597b4e367f5d0c2ee7ba49da2831b32599
jitendraMadaan <94460689+jitendraMadaan@users.noreply.github.com>
2024-12-05 18:48:32 +0530

    Update HelloTest.java

--- reporepo007/src/reporepoTestPack/HelloTest.java
+++ reporepo007/src/reporepoTestPack/HelloTest.java
@@ -5,7 +5,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("hello reporepo");                     
+		System.out.print("hello reporepo updated");                     
    
 	}
 
